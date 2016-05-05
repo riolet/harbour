40292f6225006a90463ba75854479aec5c248788
Rohana Rezel <rrezel@bestbuycanada.ca>
2016-05-05 13:17:35 -0700

    Adding bootstrap

--- harbour.py
+++ harbour.py
@@ -22,7 +22,7 @@ html_template="""
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Harbour - {{page_title}}/title>
+    <title>Harbour - {page_title}</title>
 
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
