eca12213267c4e59bd7bdeab9ca0e6be7ecb7ff2
Rohana Rezel <rrezel@bestbuycanada.ca>
2016-05-05 13:13:03 -0700

    Adding bootstrap

--- harbour.py
+++ harbour.py
@@ -11,16 +11,49 @@ urls = (
     '/drone-harbour-run', 'DroneHarbourRun'
 )
 
+html_template="""
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Harbour - {{page_title}}/title>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<div class="page-header">
+        <h1>{{page_title}}</h1>
+</div>
+<div class="row">
+	{{page_content}}
+</div>
+</body>
+</html>
+"""
 
 class index:
     def GET(self):
         # Create a UDS socket
         text = ""
         with requests_unixsocket.monkeypatch():
-            # Access /path/to/page from /tmp/profilesvc.sock
-            r = requests.get('http+unix://%2Fvar%2Frun%2Fdocker.sock/containers/json')
+            # Access /path/to/page from /tmp/Labelsprofilesvc.sock
+            r = requests.get('http+unix://%2Fvar%2Frun%2Fdocker.sock/containers/json?all=1')
             containers = r.json()
-            col_heads = containers[0].keys()
+            col_heads = ["Name", "Image", "Status", "Created", "Labels", "Ports"]
             text = "<table>"
             text += "<tr>"
             for col_head in col_heads:
@@ -32,8 +65,7 @@ class index:
                     text += "<td>" + str(container[col_head]) + "</td>"
                 text += "</tr>"
             text += "</table>"
-            return text
-            #return json2html.convert(r.json())
+            return html_template.format(page_title="Containers", page_content=text)
         return "Unknown Error"
 
 
