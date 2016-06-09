e7b673e3d07103d746f2fe2cff9f30254bc3566a
Rohana Rezel <rrezel@bestbuycanada.c>
2016-06-09 13:05:26 -0700

    Merge branch 'master' into drone_py
    # Conflicts:
    #	harbour.py

--- harbour.py
+++ harbour.py
@@ -107,7 +107,6 @@ class containers:
                 text += "<tr>"
                 for col_head in col_heads:
                     val = ""
-                    if col_head == "Ports":
                     if col_head == 'Branch':
                         if container is not None and 'Labels' in container and container['Labels'] is not None and 'branch' in container['Labels']:
                             val = container['Labels']['branch']
