364a09f7d92e906ba7bbd7a3ae6c31237fca39bd
Rohana Rezel <rrezel@bestbuycanada.c>
2016-06-13 17:01:16 -0700

    Do drone.io deploys on isolated network

--- harbour.py
+++ harbour.py
@@ -7,82 +7,18 @@ from docker import Client
 BASE_URL = 'unix://var/run/docker.sock'
 
 urls = (
-    '/', 'containers',
-    '/containers', 'containers',
+    '/', 'Containers',
+    '/containers', 'Containers',
     '/run', 'run',
     '/drone-harbour-run', 'DroneHarbourRun',
     '/action', 'action',
 )
 
-html_template = """
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Harbour - {page_title}</title>
-
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
-</head>
-<body>
-<div class="content">
-    <div class="navbar navbar-inverse">
-          <div class="container-fluid">
-            <div class="navbar-header">
-              <a href="../" class="navbar-brand">Harbour</a>
-              <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-            </div>
-          </div>
-
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li><a href="/containers">Containers</a></li>
-                </ul>
-            </div>
-    </div>
-    <div class="row-fluid">
-        <div class="col-md-12">
-            <div class="page-header">
-                    <h1>{page_title}</h1>
-            </div>
-        </div>
-    </div>
-    <div class="row-fluid">
-        {page_content}
-    </div>
-</div>
-</body>
-</html>
-"""
+html_template = ""
+render = web.template.render('templates/', base='layout')
 
 
-# def list_files(startpath):
-#     for root, dirs, files in os.walk(startpath):
-#         level = root.replace(startpath, '').count(os.sep)
-#         indent = ' ' * 4 * (level)
-#         print('{}{}/'.format(indent, os.path.basename(root)))
-#         subindent = ' ' * 4 * (level + 1)
-#         for f in files:
-#             print('{}{}'.format(subindent, f))
-
-class containers:
+class Containers:
     def GET(self):
         # Create a UDS socket
         text = ""
@@ -90,66 +26,10 @@ class containers:
             cli = Client(base_url='unix://var/run/docker.sock')
             # Access /path/to/page from /tmp/Labelsprofilesvc.sock
             containers = cli.containers()
-            col_heads = ["Names", "Image", "Status", "Created", "Ports", "Manage"]
-            text = """
-            <div class="col-md-12">
-                <table class="table">
-                    <thead>
-                        <tr>"""
-            for col_head in col_heads:
-                text += "<th>" + col_head + "</th>"
-            text += "</tr></thead><tbody>"
-            name = None
-            for container in containers:
-                text += "<tr>"
-                for col_head in col_heads:
-                    val = ""
-                    if col_head == 'Branch':
-                        if container is not None and 'Labels' in container and container[
-                            'Labels'] is not None and 'branch' in container['Labels']:
-                            val = container['Labels']['branch']
-                    elif col_head == "Ports":
-                        ports = container['Ports']
-                        count = 0
-                        for port in ports:
-                            if count > 0:
-                                val += "<br/>"
-                            if 'PublicPort' in port:
-                                val += str(port['PublicPort']) + " -> "
-                            val += str(port['PrivatePort'])
-                            count += 1
-                    elif col_head == "Names":
-                        if container and col_head in container and container[col_head]:
-                            Names = container[col_head]
-                            count = 0
-                            for name in Names:
-                                if count > 0:
-                                    val += "<br/>"
-                                val += str(name)
-                                count += 1
-                    elif col_head == "Manage":
-                        val += """
-                                <form action="/action"><select id="sel_id" name="action"  onchange="this.form.submit();">
-                                <option value="-1">Select</option>
-                                <option value="start">Start</option>
-                                <option value="stop">Stop</option>
-                                <option value="restart">Restart</option>
-                                <option value="logs">Logs</option>
-                                <option value="top">Top</option>
-                                <option value="inspect">Inspect</option>
-                                </select>
-                                <input type="hidden" name="name" value="{name}">
-                                <input type="hidden" name="id" value="{id}">
-                                </form>
-                            """.format(name=name, id=container['Id'])
-                    else:
-                        val = str(container[col_head])
-                    text += "<td>" + val + "</td>"
-
-                text += "</tr>"
-            text += "</tbody></table></div>"
-            return html_template.format(page_title="Containers", page_content=text)
+            col_heads = ["Names", "Image", "Tag", "Status", "Created", "Ports", "Labels", "Manage"]
+            return render.containers(title="Containers", col_heads=col_heads, containers=containers)
         except Exception as e:
+            traceback.print_exc()
             return "Unknown Error: " + str(e)
 
 
@@ -177,11 +57,12 @@ class DroneHarbourRun:
         print data
         registry = data['registry']
         image = data['image']
-        tag = data['tag']
+        tag = ('tag' in data and data['tag']) or None
         envs = data['env'] or []
         ports = data['ports'] or []
         port_bindings = data['port_bindings'] or {}
-        links = data['links'] or {}
+        #links = data['links'] or {}
+        links = {}
         publish_all_ports = data['publish_all_ports'] or False
 
         cli = Client(base_url='unix://var/run/docker.sock')
@@ -189,6 +70,11 @@ class DroneHarbourRun:
         cli.pull("{registry}:5000/{image}:latest".format(registry=registry, image=image))
 
         name = image
+
+        # try:
+        #     cli.create_network(name="network1", driver="bridge")
+        # except:
+        #     text += "Network not created"
 
         try:
             cli.stop(name)
@@ -231,7 +117,8 @@ class DroneHarbourRun:
                                                  labels=labels,
                                                  host_config=cli.create_host_config(port_bindings=port_bindings,
                                                                                     links=links,
-                                                                                    publish_all_ports=publish_all_ports))
+                                                                                    publish_all_ports=publish_all_ports,
+                                                                                    network_mode="network2"))
             text = new_container['Id']
             try:
                 cli.start(name)
@@ -258,47 +145,45 @@ class action:
 
         id = data.id
         cli = Client(base_url='unix://var/run/docker.sock')
-        if data.action == "start":
+
+        if action in ["start", "stop", "restart"]:
             try:
-                cli.start(id)
-                text += "Image started"
+                if data.action == "start":
+                    cli.start(id)
+                elif data.action == "stop":
+                    cli.stop(id)
+                else:
+                    cli.restart(id)
+                return render.notification(title="Harbour - {action} for {name}".format(action=action, name=name),
+                                           message="Image {name} {action}ed successfully".format(action=action, name=name),
+                                           status="success")
             except Exception as e:
                 return error_out("Image not started", e)
-        elif data.action == "stop":
+        elif action == "logs":
             try:
-                cli.stop(id)
-                text += "Image stopped"
-            except Exception as e:
-                return error_out("Image not stopped", e)
-        elif data.action == "restart":
-            try:
-                cli.restart(id)
-                text += "Image restarted"
-            except Exception as e:
-                return error_out("Image not restarted", e)
-        elif data.action == "logs":
-            try:
-                text += "<pre>" + cli.logs(id) + "</pre>"
+                return "<pre>" + cli.logs(id) + "</pre>"
             except Exception as e:
                 return error_out("Unable to get image logs", e)
-        elif data.action == "inspect":
+        elif action == "inspect":
             try:
-                text += "<pre>" + json2html.convert(json=json.dumps(cli.inspect_container(id))) + "</pre>"
+                return "<pre>" + json2html.convert(json=json.dumps(cli.inspect_container(id))) + "</pre>"
             except Exception as e:
                 return error_out("Unable to inspect container", e)
-        elif data.action == "top":
+        elif action == "top":
             try:
                 top = cli.top(id)
-                toplist = []
+                processes = []
+                col_heads = top['Titles']
                 for process in top['Processes']:
                     topdic = {}
                     header_idx=0
                     for header in top['Titles']:
                         topdic[header]=process[header_idx]
                         header_idx += 1
-                    toplist += [topdic]
-
-                text += "<pre>" + json2html.convert(json=json.dumps({'Top':toplist})) + "</pre>"
+                    processes += [topdic]
+                return render.top(title="{action} for {name}".format(action=action, name=name),
+                                  col_heads=col_heads,
+                                  processes=processes)
             except Exception as e:
                 return error_out("Unable to run top con container", e)
         return html_template.format(page_title="{action} for {name}".format(action=action, name=name),
